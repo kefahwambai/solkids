10fe5ba234053adb40463980e8f5dea665a92193
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-04-13 11:08:11 +0300

    added analystics and speed

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'react-html5video/dist/styles.css'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics/>
+    <SpeedInsights/>
     <App />
   </React.StrictMode>
 );
