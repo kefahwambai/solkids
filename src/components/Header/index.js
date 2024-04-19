b303c1a344e2486494a8ab2349f616a26d9527af
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-04-19 10:50:13 +0300

    added scrolltrigger

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useRef} from 'react';
 import "./header.css"
 import { Link } from 'react-router-dom';
 import kids from "../../Assets/Homepage/solchick.png";
@@ -13,22 +13,8 @@ import stars from "../../Assets/Books/written in stars.png"
 import solmic from "../../Assets/Homepage/solmic.PNG"
 import pizza from "../../Assets/definition/pizzakids.png"
 import kich from "../../Assets/Homepage/solchick.png"
-// import galo from "../../Assets/slideshow/DSC02924.png"
-// import galt from "../../Assets/slideshow/DSC02941.png"
-// import galtr from "../../Assets/slideshow/DSC02942.png"
-// import galf from "../../Assets/slideshow/DSC02969.png"
-// import galfv from "../../Assets/slideshow/DSC02970.png"
-// import gals from "../../Assets/slideshow/DSC02979.png"
-// import galsv from "../../Assets/slideshow/DSC02996.png"
-// import gale from "../../Assets/slideshow/DSC03029.png"
-// import solfest from "../../Assets/Events/dec2022.jpeg"
-// import solfest23 from "../../Assets/Events/solfest23.jpeg"
+import useScrollTriggeredCountUp from './useScrollTriggeredCountUp';
 
-
-// const events = [
-//   { id: 1, title: 'SolKids Festival 2022', image: solfest },
-//   { id: 2, title: 'Solkids Festival 2023', image: solfest23 },
-// ];
 const products = [
   { id: 1, name: "Hibiscus Oak", category: "Best seller", price: "Kshs 700", image: oak },
   { id: 2, name: "Towering Twiga", price: "Kshs 700", image: twiga },
@@ -40,6 +26,19 @@ export default function CalendarComponent({ handleClick }) {
   const [filter, setFilter] = useState('*'); 
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [quantity, setQuantity] = useState(0);
+  const childrenReachedRef = useRef(null);
+  const solkidsMembersRef = useRef(null);
+  const contentCatalogueRef = useRef(null);
+  const awardsRef = useRef(null);
+  const count = useScrollTriggeredCountUp(solkidsMembersRef, 40);
+  const countkids = useScrollTriggeredCountUp(childrenReachedRef, 4);
+  const countcont = useScrollTriggeredCountUp(contentCatalogueRef, 20);
+  const awardcount = useScrollTriggeredCountUp(awardsRef, 2);
+
+  useScrollTriggeredCountUp(childrenReachedRef, 4);
+  useScrollTriggeredCountUp(solkidsMembersRef, 40);
+  useScrollTriggeredCountUp(contentCatalogueRef, 20);
+  useScrollTriggeredCountUp(awardsRef, 2);
 
   const handleQuantityChange = (event) => {
     setQuantity(event.target.value);
@@ -154,85 +153,6 @@ export default function CalendarComponent({ handleClick }) {
           </div>
         </div>
       </div>
-          {/* <div className="container-xxl py-5">
-            <div className="container">
-              <div className="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style={{ maxWidth: "600px" }}>
-                <h1 className="mb-3">About us</h1>
-              </div>
-              <div className="row g-4">
-                <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-                  <div className="facility-item">
-                    <div className="facility-icon bg-primary">
-                      <span className="bg-primary"></span>
-                      <i className="fas fa-child fa-3x text-primary"></i>
-                      <span className="bg-primary"></span>
-                    </div>
-                    <div className="facility-text bg-primary">
-                      <h3 className="text-primary mb-3">Variety of Kid content</h3>
-                      <p className="mb-0"> We champion Afro-futuristic sci-fi narratives that address pollution and climate change, amplifying African voices worldwide through engaging storytelling.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-                  <div className="facility-item">
-                    <div className="facility-icon bg-success">
-                      <span className="bg-success"></span>
-                      <i className="fas fa-futbol fa-3x text-success"></i>
-                      <span className="bg-success"></span>
-                    </div>
-                    <div className="facility-text bg-success">
-                      <h3 className="text-success mb-3">Children Empowerment</h3>
-                      <p className="mb-0">Relatable context that empowers through content that is inclusive, representative and diverse.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-                  <div className="facility-item">
-                    <div className="facility-icon bg-warning">
-                      <span className="bg-warning"></span>
-                      <i className="fa-solid fa-users fa-3x text-warning"></i>
-                      <span className="bg-warning"></span>
-                    </div>
-                    <div className="facility-text bg-warning">
-                      <h3 className="text-warning mb-3">Leadership</h3>
-                      <p className="mb-0">Raising leaders through content, books and stories that inspire and motivate.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-                  <div className="facility-item">
-                    <div className="facility-icon bg-info">
-                      <span className="bg-info"></span>
-                      <i className="fas fa-chalkboard-teacher fa-3x text-info"></i>
-                      <span className="bg-info"></span>
-                    </div>
-                    <div className="facility-text bg-info">
-                      <h3 className="text-info mb-3">Positive Learning</h3>
-                      <p className="mb-0">High quality content that is accessible to all children on all platforms translated into Kiswahili, French and local languages.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
-        {/* <div className='founderss'>
-            <div>
-                <img data-aos="fade-right" data-aos-duration="3000" className='pizza' src={pizzakids} alt="kids making pizza" />
-                <div data-aos="fade-down" data-aos-easing="linear" data-aos-duration="2000">  
-                    <p className='greenp'> Sol Kids is an initiative led by Sauti Sol, offering an innovative platform that curates affordable and award-winning Pan African content.</p>
-                    <p className='rment'> Our goal is to nurture a generation of children who embrace their African heritage. </p>
-                    <p className='sment'> With a mantra of "Stories by Us," our mission revolves around L.E.A (Leadership, Empowerment, Accessibility).</p>
-                    <p className='yellowp'> We aim to promote literacy, empowerment, and accessibility through culturally relevant content, empowering children to appreciate their past, present, and future.</p>                   
-                </div>
-                <img data-aos="fade-up" data-aos-duration="2000" className='solhen' src={chick} alt="realsolchick" />
-            </div>          
-        </div>
-        <div >
-          <span className="dot"></span>
-          <span className="dot"></span>
-          <span className="dot"></span>
-          <span className="dot"></span>
-        </div> */}
     </section>
       <section id="frontshop">
       <div className="products-box">
@@ -335,16 +255,6 @@ export default function CalendarComponent({ handleClick }) {
         <p>Check our gallery from the recent events</p>
       </div>
     </div>
-    <div className="owl-carousel gallery-carousel">
-      {/* <a href="../../Assets/slideshow/DSC02924.png" className="venobox" data-gall="gallery-carousel"><img src={galo} alt=""/></a>
-      <a href="../../Assets/slideshow/DSC02941.png" className="venobox" data-gall="gallery-carousel"><img src={galt} alt=""/></a>
-      <a href="../../Assets/slideshow/DSC03029.png" className="venobox" data-gall="gallery-carousel"><img src={gale} alt=""/></a>
-      <a href="../../Assets/slideshow/DSC02969.png" className="venobox" data-gall="gallery-carousel"><img src={galf} alt=""/></a>
-      <a href="../../Assets/slideshow/DSC02970.png" className="venobox" data-gall="gallery-carousel"><img src={galfv} alt=""/></a>
-      <a href="../../Assets/slideshow/DSC02979.png" className="venobox" data-gall="gallery-carousel"><img src={gals} alt=""/></a>
-      <a href="../../Assets/slideshow/DSC02996.png" className="venobox" data-gall="gallery-carousel"><img src={galsv} alt=""/></a>
-      <a href="../../Assets/slideshow/DSC02942.png" className="venobox" data-gall="gallery-carousel"><img src={galtr} alt=""/></a> */}
-    </div>
 
     </section>
       <section id="planner" >
@@ -359,14 +269,14 @@ export default function CalendarComponent({ handleClick }) {
               <div className="col-lg-6">
                 <div className="row">
                   <div className="col-12">
-                    <div className="count-area-content percentage">
-                      <div className="count-digit">4</div>
+                    <div className="count-area-content percentage" ref={childrenReachedRef}>
+                      <div className="count-digit">{countkids}</div>
                       <div className="count-title">Children reached</div>
                     </div>
                   </div>
                   <div className="col-12">
-                    <div className="count-area-content">
-                      <div className="count-digit">40+</div>
+                    <div className="count-area-content" ref={solkidsMembersRef}>
+                      <div className="count-digit">{count}+</div>
                       <div className="count-title">Current Solkids members</div>
                     </div>
                   </div>
@@ -375,14 +285,14 @@ export default function CalendarComponent({ handleClick }) {
               <div className="col-lg-6">
                 <div className="row">
                   <div className="col-12">
-                    <div className="count-area-content new-students">
-                      <div className="count-digit">20+</div>
+                    <div className="count-area-content new-students" ref={contentCatalogueRef}>
+                      <div className="count-digit">{countcont}+</div>
                       <div className="count-title">Content Catalogue </div>
                     </div>
                   </div>
                   <div className="col-12">
-                    <div className="count-area-content">
-                      <div className="count-digit">2</div>
+                    <div className="count-area-content" ref={awardsRef}>
+                      <div className="count-digit">{awardcount}</div>
                       <div className="count-title">Awards</div>
                     </div>
                   </div>
